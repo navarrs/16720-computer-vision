f547f6bf7362f32bc721f6e2829c91ce7c92a355
navarrs <ingrid.navarro.an@gmail.com>
2020-10-01 23:26:07 -0500

    started q2.1.4

---
+++ hw2/python/HarryPotterize.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import skimage.io 
+import skimage.color
+from opts import get_opts
+
+#Import necessary functions
+
+
+
+#Write script for Q2.2.4
+opts = get_opts()
---
+++ hw2/python/q2_1_4.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+from matchPics import matchPics
+from helper import plotMatches
+from opts import get_opts
+
+
+def ablation(opts, cv_cover, cv_desk):
+  sigmas = np.arange(0.7, 1.0, 0.1)
+  ratios = np.arange(0.1, 1.0, 0.1)
+  for s in sigmas:
+    for r in ratios:
+      opts.ratio = r
+      opts.sigma = s
+      print("Using ratio {:.2f}, sigma {:.2f}".format(r, s))
+      
+      cvc = cv_cover.copy()
+      cvd = cv_desk.copy()
+      
+      matches, locs1, locs2 = matchPics(cvc, cvd, opts)
+      plotMatches(cv_cover, cv_desk, matches, locs1, locs2, opts)
+
+opts = get_opts()
+
+cv_cover = cv2.imread('../data/cv_cover.jpg')
+cv_desk = cv2.imread('../data/cv_desk.png')
+
+matches, locs1, locs2 = matchPics(cv_cover, cv_desk, opts)
+
+#display matched features
+plotMatches(cv_cover, cv_desk, matches, locs1, locs2, opts)
+
+# Ablation study 
+# ablation(opts, cv_cover, cv_desk)
+
+    
