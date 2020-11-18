5feb29029dd2ba5873d7c141fd2026a2020250b2
navarrs <ingrid.navarro.an@gmail.com>
2020-11-18 00:42:05 -0600

    added: q223

--- hw5/python/run_q2.py
+++ hw5/python/run_q2.py
@@ -38,23 +38,23 @@ print("{}, {:.2f}".format(params['boutput'].sum(),params['Woutput'].std()**2))
 # Q 2.2.1
 # implement sigmoid
 test = sigmoid(np.array([-1000,1000]))
-print('should be zero and one\t',test.min(),test.max())
+print('should be zero and one:\t',test.min(),test.max())
 # implement forward
-h1 = forward(x,params,'layer1')
+h1 = forward(x, params, 'layer1')
 print(h1.shape)
 # Q 2.2.2
 # implement softmax
-probs = forward(h1,params,'output',softmax)
+probs = forward(h1, params, 'output', softmax)
 # make sure you understand these values!
 # positive, ~1, ~1, (40,4)
-print(probs.min(),min(probs.sum(1)),max(probs.sum(1)),probs.shape)
+print(probs.min(), min(probs.sum(1)), max(probs.sum(1)), probs.shape)
 
 # Q 2.2.3
 # implement compute_loss_and_acc
 loss, acc = compute_loss_and_acc(y, probs)
 # should be around -np.log(0.25)*40 [~55] and 0.25
 # if it is not, check softmax!
-print("{}, {:.2f}".format(loss,acc))
+print("{}, {:.2f}".format(loss, acc))
 
 # here we cheat for you
 # the derivative of cross-entropy(softmax(x)) is probs - 1[correct actions]
@@ -64,82 +64,82 @@ delta1[np.arange(probs.shape[0]),y_idx] -= 1
 # we already did derivative through softmax
 # so we pass in a linear_deriv, which is just a vector of ones
 # to make this a no-op
-delta2 = backwards(delta1,params,'output',linear_deriv)
-# Implement backwards!
-backwards(delta2,params,'layer1',sigmoid_deriv)
+# delta2 = backwards(delta1, params, 'output', linear_deriv)
+# # Implement backwards!
+# backwards(delta2,params,'layer1',sigmoid_deriv)
 
-# W and b should match their gradients sizes
-for k,v in sorted(list(params.items())):
-    if 'grad' in k:
-        name = k.split('_')[1]
-        print(name,v.shape, params[name].shape)
+# # W and b should match their gradients sizes
+# for k,v in sorted(list(params.items())):
+#     if 'grad' in k:
+#         name = k.split('_')[1]
+#         print(name,v.shape, params[name].shape)
 
-# Q 2.4
-batches = get_random_batches(x,y,5)
-# print batch sizes
-print([_[0].shape[0] for _ in batches])
-batch_num = len(batches)
+# # Q 2.4
+# batches = get_random_batches(x,y,5)
+# # print batch sizes
+# print([_[0].shape[0] for _ in batches])
+# batch_num = len(batches)
 
-# WRITE A TRAINING LOOP HERE
-max_iters = 500
-learning_rate = 1e-3
-# with default settings, you should get loss < 35 and accuracy > 75%
-for itr in range(max_iters):
-    total_loss = 0
-    avg_acc = 0
-    for xb,yb in batches:
-        pass
-        # forward
+# # WRITE A TRAINING LOOP HERE
+# max_iters = 500
+# learning_rate = 1e-3
+# # with default settings, you should get loss < 35 and accuracy > 75%
+# for itr in range(max_iters):
+#     total_loss = 0
+#     avg_acc = 0
+#     for xb,yb in batches:
+#         pass
+#         # forward
 
-        # loss
-        # be sure to add loss and accuracy to epoch totals 
+#         # loss
+#         # be sure to add loss and accuracy to epoch totals 
 
-        # backward
+#         # backward
 
-        # apply gradient
+#         # apply gradient
 
-        ##########################
-        ##### your code here #####
-        ##########################
+#         ##########################
+#         ##### your code here #####
+#         ##########################
 
         
-    if itr % 100 == 0:
-        print("itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(itr,total_loss,avg_acc))
+#     if itr % 100 == 0:
+#         print("itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(itr,total_loss,avg_acc))
 
 
-# Q 2.5 should be implemented in this file
-# you can do this before or after training the network. 
+# # Q 2.5 should be implemented in this file
+# # you can do this before or after training the network. 
 
-##########################
-##### your code here #####
-##########################
+# ##########################
+# ##### your code here #####
+# ##########################
 
-# save the old params
-import copy
-params_orig = copy.deepcopy(params)
+# # save the old params
+# import copy
+# params_orig = copy.deepcopy(params)
 
-eps = 1e-6
-for k,v in params.items():
-    if '_' in k: 
-        continue
-    # we have a real parameter!
-    # for each value inside the parameter
-    #   add epsilon
-    #   run the network
-    #   get the loss
-    #   compute derivative with central diffs
+# eps = 1e-6
+# for k,v in params.items():
+#     if '_' in k: 
+#         continue
+#     # we have a real parameter!
+#     # for each value inside the parameter
+#     #   add epsilon
+#     #   run the network
+#     #   get the loss
+#     #   compute derivative with central diffs
     
-    ##########################
-    ##### your code here #####
-    ##########################
+#     ##########################
+#     ##### your code here #####
+#     ##########################
 
-total_error = 0
-for k in params.keys():
-    if 'grad_' in k:
-        # relative error
-        err = np.abs(params[k] - params_orig[k])/np.maximum(np.abs(params[k]),np.abs(params_orig[k]))
-        err = err.sum()
-        print('{} {:.2e}'.format(k, err))
-        total_error += err
-# should be less than 1e-4
-print('total {:.2e}'.format(total_error))
+# total_error = 0
+# for k in params.keys():
+#     if 'grad_' in k:
+#         # relative error
+#         err = np.abs(params[k] - params_orig[k])/np.maximum(np.abs(params[k]),np.abs(params_orig[k]))
+#         err = err.sum()
+#         print('{} {:.2e}'.format(k, err))
+#         total_error += err
+# # should be less than 1e-4
+# print('total {:.2e}'.format(total_error))
